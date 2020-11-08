7136c2f0dd5a71deda0692fbde6cd285ecaa7546
jdtran96 <58291826+jdtran96@users.noreply.github.com>
2020-11-07 23:20:39 -0800

    took out inital layout, made regular layout

---
+++ app/src/main/java/com/example/flexqueue/MainPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.flexqueue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+    }
+}
